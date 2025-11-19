deb6b583c945aeab683c38c428c089c3d7caf2f0
tphamer8 <tm.phamer@gmail.com>
2025-11-19 12:32:15 -0500

    added some shopper statistics and buttons

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,12 +3,45 @@
 import styles from "./page.module.css"
 import React from "react"
 
-
 export default function DashboardPage() {
+  const [redraw, forceRedraw] = React.useState(0)
+
+  // helper function that forces React app to redraw whenever this is called.
+  function andRefreshDisplay() {
+    forceRedraw(redraw + 1)
+  }
+
+
   return (
     <div>
       <main>
-        <h1>Dashboard page</h1>
+        <h4 className={styles.pageHeader}>Dashboard page</h4>
+        <h4 className={styles.sectionHeader}>Highlight Statistics</h4>
+        <div className={styles.statsContainer}>
+          <div className={styles.statItem}>
+            <h2>Receipts</h2>
+            <p>5</p>
+          </div>
+          <div className={styles.statItem}>
+            <h2>Shopping Lists</h2>
+            <p>3</p>
+          </div>
+          <div className={styles.statItem}>
+            <h2>Purchases</h2>
+            <p>120</p>
+          </div>
+          <div className={styles.statItem}>
+            <h2>Money Saved</h2>
+            <p>$1000</p>
+          </div>
+        </div>
+
+        <h4 className={styles.sectionHeader}>Recent Receipts List</h4>
+        <h4 className={styles.sectionHeader}>Recent Shopping List</h4>
+        <button onClick={andRefreshDisplay}>View Receipts</button>
+        <button onClick={andRefreshDisplay}>View Shopping Lists</button>
+        <button onClick={andRefreshDisplay}>View Stores</button>
+        <button onClick={andRefreshDisplay}>Search Recent Purchases</button>
       </main>
     </div>
   )
