12586b841eba5e65590c4452a5941cfc3c825454
asuyer-wpi <asuyer@wpi.edu>
2025-11-24 13:49:22 -0500

    Added basePath to config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export"
+  output: "export",
+  basePath: "/shopcomp",
 };
 
 export default nextConfig;
