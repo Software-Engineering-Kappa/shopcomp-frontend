b9a2307828f9cf7447846bf6a5a3906ace8055aa
asuyer-wpi <asuyer@wpi.edu>
2025-11-24 11:36:34 -0500

    Updated header to clear auth token and redirect to login page

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,11 +1,16 @@
 import Link from "next/link";
 import styles from "./header.module.css";
+import { unsetAuthorizationToken } from "../axiosClient";
 
 export default function Header() {
-    return (
-        <header className={styles.header}>
-            <Link href="/dashboard" className={styles.shopCompTitle}><h1>ShopComp</h1></Link>
-            <Link href="/" className={styles.logoutButton}>Logout</Link>
-        </header>
-    )
+  function handleLogout() {
+    unsetAuthorizationToken()
+  }
+
+  return (
+    <header className={styles.header}>
+      <Link href="/dashboard" className={styles.shopCompTitle}><h1>ShopComp</h1></Link>
+      <Link href="/login" className={styles.logoutButton} onClick={handleLogout}>Logout</Link>
+    </header>
+  )
 }
