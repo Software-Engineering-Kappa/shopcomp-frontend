9baddcda797c8f49ae22f6b73406cf970eb3d6c1
asuyer-wpi <asuyer@wpi.edu>
2025-12-07 12:47:51 -0500

    Created navigation buttons on the header bar with icons

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -6,10 +6,17 @@ import { unsetAuthorizationTokens } from "../axiosClient";
 
 import React from "react"
 import { getIdToken } from "../axiosClient"
-import { useRouter } from "next/navigation"
+import { useRouter, usePathname } from "next/navigation"
+
+import DashboardIcon from '@mui/icons-material/Dashboard'
+import ReceiptIcon from '@mui/icons-material/Receipt'
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
+import StoreIcon from '@mui/icons-material/Store'
+import LocalOfferIcon from '@mui/icons-material/LocalOffer'
 
 export default function Header() {
   const router = useRouter()
+  const pathname = usePathname()
   const [username, setUsername] = React.useState("")
   const [role, setRole] = React.useState("")
 
@@ -33,6 +40,15 @@ export default function Header() {
     localStorage.removeItem("role")
   }
 
+  // Navigation links
+  const navLinks = [
+    { href: "/dashboard/", label: "Dashboard", icon: <DashboardIcon/> },
+    { href: "/receipts/", label: "Receipts", icon: <ReceiptIcon/> },
+    { href: "/shopping-lists/", label: "Shopping Lists", icon: <ShoppingCartIcon/> },
+    { href: "/stores/", label: "Stores", icon: <StoreIcon/> },
+    { href: "/purchases/", label: "Purchases", icon: <LocalOfferIcon/> },
+  ]
+
   return (
     <header className={styles.header}>
       <Link href="/dashboard" className={styles.shopCompTitle}>
@@ -45,10 +61,27 @@ export default function Header() {
           <h1>ShopComp</h1>
         </span>
       </Link>
-      <div className={styles.logoutContainer}>
-        <b>{username}</b> <br/>
-        <b>{role}</b> <br/>
-        <Link href="/login" className={styles.logoutButton} onClick={handleLogout}>Logout</Link>
+      <div className={styles.rightSectionContainer}>
+        <div className={styles.navigationButtonContainer}>
+          {navLinks.map(link => (
+            <Link
+              key={link.href}
+              href={link.href}
+              className={
+                styles.navigationButton + (pathname === link.href ? " " + styles.active : "")
+              }
+            >
+              {link.label}
+              {link.icon}
+            </Link>
+          ))}
+        </div>
+        <div className={styles.logoutContainer}>
+          <b>{username}</b> <br />
+          <b>{role}</b> <br />
+          <Link href="/login" className={styles.logoutButton} onClick={handleLogout}>Logout</Link>
+        </div>
+
       </div>
     </header>
   )
