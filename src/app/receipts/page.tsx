ed69f923be36107bdae68e42ad81c446624831ba
asuyer-wpi <asuyer@wpi.edu>
2025-12-10 17:14:20 -0500

    Made AI form only show if receipt has no items

--- src/app/receipts/page.tsx
+++ src/app/receipts/page.tsx
@@ -15,38 +15,40 @@ export default function ReceiptsPage() {
   const [receipt, setReceipt] = React.useState<Receipt>()
 
   const handleEditReceiptClick = () => {
-      if (receiptId >= 0 && !createReceipt)
-          setEditReceipt(true);
+    if (receiptId >= 0 && !createReceipt)
+      setEditReceipt(true);
   }
-  
+
   const handleCreateReceiptClick = () => {
-      if (!editReceipt)
-          setCreateReceipt(true);
+    if (!editReceipt)
+      setCreateReceipt(true);
   }
 
   return (
-    <div> 
+    <div>
       <Header />
-        <main>
-          <h1>Receipts</h1>
-          <ReceiptSearch createReceipt={createReceipt} editReceipt={editReceipt} setReceiptId={setReceiptId}/>
-          <button type="button" className="edit-receipt" onClick={() => handleEditReceiptClick()}>Edit Receipt</button>
-          <button type="button" className="create-receipt" onClick={() => handleCreateReceiptClick()}>Create Receipt</button>
+      <main>
+        <h1>Receipts</h1>
+        <ReceiptSearch createReceipt={createReceipt} editReceipt={editReceipt} setReceiptId={setReceiptId} />
+        <button type="button" className="edit-receipt" onClick={() => handleEditReceiptClick()}>Edit Receipt</button>
+        <button type="button" className="create-receipt" onClick={() => handleCreateReceiptClick()}>Create Receipt</button>
       </main>
-      <CreateReceiptForm displayed={createReceipt} setDisplayed={setCreateReceipt} setEditReceiptDisplayed={setEditReceipt} setReceiptId={setReceiptId}/>
-      <EditReceiptForm 
+      <CreateReceiptForm displayed={createReceipt} setDisplayed={setCreateReceipt} setEditReceiptDisplayed={setEditReceipt} setReceiptId={setReceiptId} />
+      <EditReceiptForm
         displayed={editReceipt}
         setDisplayed={setEditReceipt}
         receiptId={receiptId}
         receipt={receipt}
         setReceipt={setReceipt}
       />
-      <br/>
-      <AnalyzeWithAIForm 
-        receiptId={receiptId}
-        receipt={receipt}
-        setReceipt={setReceipt}
-      />
+      <br />
+      {receipt && receipt.items.length === 0 &&
+        <AnalyzeWithAIForm
+          receiptId={receiptId}
+          receipt={receipt}
+          setReceipt={setReceipt}
+        />
+      }
     </div>
   )
 }
